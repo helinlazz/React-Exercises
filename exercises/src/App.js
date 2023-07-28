4e890ece51d535f66ccd5dec131a6db55380082e
Mirka <mirkamedaglini@hotmail.it>
2023-07-28 13:14:48 +0200

    Exercise completed

--- exercises/src/App.js
+++ exercises/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import ClickCounter from "./ClickCounter";
 
 
 export default function App () {
     return (
         <>
-
+            <ClickCounter />
         </>
 
     )
