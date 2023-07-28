6d42994572d12c763ad2c6bd7f074582a49d6cab
Mirka <mirkamedaglini@hotmail.it>
2023-07-28 13:14:25 +0200

    Exercise completed

--- exercises/src/ClickCounter.js
+++ exercises/src/ClickCounter.js
@@ -4,7 +4,7 @@ export default function ClickCounter () {
     const [counter, setCounter] = useState(0);
 
     const handleClickButton = () => {
-        setCounter[counter => counter + 1];
+        setCounter(counter + 1);
     }
 
     return (
