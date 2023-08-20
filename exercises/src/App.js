982a3f3fdc792ebb8039f7c79a11b905b0470f04
Mirka <mirkamedaglini@hotmail.it>
2023-08-20 15:34:25 +0200

    Exercise completed

--- exercises/src/App.js
+++ exercises/src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
 import GithubUser from "./GithubUser";
+import GithubUserList from "./GithubUserList";
 
 
 export default function App () {
 };
 return (
-    <GithubUser username="helinlazz" />
+    <>
+        <GithubUser username="helinlazz" />
+        <GithubUserList />
+    </>
 );
