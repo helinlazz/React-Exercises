0a84aec991e92c04d70b797454350a58eb7eb660
Mirka <mirkamedaglini@hotmail.it>
2023-08-20 16:11:53 +0200

    Exercise completed

--- exercises/src/useGithubUser.js
+++ exercises/src/useGithubUser.js
@@ -2,24 +2,32 @@ import { useState, useEffect } from 'react';
 
 export default function useGithubUser (username) {
     const [user, setUser] = useState();
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState();
+
+    const fetchUser = async () => {
+        setLoading(true);
+        try {
+            const response = await fetch(`https://api.github.com/users/${username}`);
+            const data = await response.json();
+            setUser(data);
+            setLoading(false);
+        } catch (error) {
+            setError(error);
+            setLoading(false);
+        }
+    };
 
     useEffect(() => {
-        async function fetchUser () {
-            try {
-                const response = await fetch(`https://api.github.com/users/${username}`);
-                const data = await response.json();
-                setUser(data);
-            } catch (error) {
-                console.error(error);
-            }
-        }
-
         if (username) {
             fetchUser();
         }
     }, [username]);
 
-    return user;
+    return {
+        user,
+        loading,
+        error,
+        fetchUser: fetchUser
+    };
 }
-
-
