4f646d88de0fd5cd3043ce30531085e22e5096cc
Mirka <mirkamedaglini@hotmail.it>
2023-08-27 16:29:00 +0200

    Exercise completed

--- exercises/src/App.js
+++ exercises/src/App.js
@@ -1,11 +1,17 @@
-import React from "react";
-import CarDetails from "./CarDetails";
+import { FilteredList } from "./UseMemo";
 
+export function App () {
 
-export default function App () {
-};
-return (
-    <>
-        <CarDetails />
-    </>
-);
+    const nomi = [
+        { name: 'pino', id: 1, age: 20 },
+        { name: 'maria', id: 2, age: 17 },
+        { name: 'carlo', id: 3, age: 13 },
+        { name: 'letizia', id: 5, age: 34 }
+    ]
+
+    return (
+        <div>
+            <FilteredList list={nomi} />
+        </div>
+    )
+}
