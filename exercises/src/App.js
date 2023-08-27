3c88cb831fd2f4625aa3e838de647da986d11af7
Mirka <mirkamedaglini@hotmail.it>
2023-08-27 16:38:18 +0200

    Exercise completed

--- exercises/src/App.js
+++ exercises/src/App.js
@@ -1,24 +1,13 @@
-import React, { useState } from "react";
-import { LanguageContext } from "./LanguageContext";
-import { Welcome } from "./Welcome";
+import { Route, Routes } from 'react-router-dom'
+import { GetGithubUser } from './GithubUser';
 
 export default function App () {
 
-    const [language, setLanguage] = useState('eng')
-
-    const handleChangeLanguage = (event) => {
-        setLanguage(event.target.value)
-    }
-
     return (
         <div>
-            <select value={language} onChange={handleChangeLanguage}>
-                <option value='eng'>english</option>
-                <option value='ita'>italian</option>
-            </select>
-            <LanguageContext.Provider value={language}>
-                <Welcome language={language} />
-            </LanguageContext.Provider>
+            <Routes>
+                <Route path="/users/:username" element={<GetGithubUser />} />
+            </Routes>
         </div>
     )
 }
---
+++ exercises/src/GithubUser.js
@@ -0,0 +1,17 @@
+import { useParams } from "react-router-dom"
+import { useGetGithubUser } from "./UseGithubUser"
+
+export default function GetGithubUser () {
+
+    const { username } = useParams()
+
+    const { data, loading, error } = useGetGithubUser(username)
+
+    return (
+        <div>
+            {loading && <h5>Loading...</h5>}
+            {data && <h5>{data.login}, {data.name}</h5>}
+            {error && <h5>errore</h5>}
+        </div>
+    )
+}
