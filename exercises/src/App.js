37691777ce259c10416912074f2152d54d930b75
Mirka <mirkamedaglini@hotmail.it>
2023-08-31 15:30:39 +0200

    Exercise completed

--- exercises/src/App.js
+++ exercises/src/App.js
@@ -1,9 +1,10 @@
 
 import { Route, Routes } from 'react-router-dom'
-import CarDetails from './CarDetails'
+import { CarDetails } from './CarDetails'
 import { Welcome } from './welcome'
 import { Login } from './Login'
 import { NotFound } from './NotFound'
+import { FetchGithubUserList } from './GithubUserList'
 
 export default function App () {
 
@@ -14,7 +15,7 @@ export default function App () {
                 <Route path="/CarDetails" element={<CarDetails />}></Route>
                 <Route path="/Login" element={<Login />}></Route>
                 <Route path="/*" element={<NotFound />}></Route>
-                <Route path="/GithubUserList" element={<GithubUserList />} />
+                <Route path="/GithubUserList" element={<FetchGithubUserList />} />
             </Routes>
         </div>
     )
