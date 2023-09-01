c760340e7caea9425d4bd064abdeaa03b5eafdb5
Mirka <mirkamedaglini@hotmail.it>
2023-09-01 04:51:28 +0200

    Exercise completed

--- exercises/src/useGithubUser.js
+++ exercises/src/useGithubUser.js
@@ -3,12 +3,29 @@ import useSWR from 'swr'
 const fetcher = url => fetch(url).then(res => res.json())
 
 
+function useSearchUser (username) {
+    const { data, error, mutate } = useSWR(username && `https://api.github.com/users/${username}`, fetcher)
+
+    function handleRefresh () {
+        mutate()
+
+        console.log(data)
+    }
+
+    return {
+        data,
+        error,
+        onRefresh: handleRefresh,
+    }
+}
+
 export function useGetGithubUser (username) {
 
-    const { data, error } = useSWR(username && `https://api.github.com/users/${username}`, fetcher)
+    const { data, error, onRefresh } = useSearchUser(username)
 
     return {
         data: data,
         error: error,
+        onRefresh: onRefresh,
     }
 }
